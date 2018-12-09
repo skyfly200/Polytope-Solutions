7db1e774436b939b6d104dd9dfa41fbfb0c1c0d8
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-08 21:20:02 -0700

    basic landing page

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,21 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="sass">
+#app
+  font-family: 'Rajdhani', Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: white
+  background-color: black
+  min-height: 100vh
+
+#nav
+  padding: 30px
+  a
+    font-weight: bold
+    color: #2c3e50
+    &.router-link-exact-active
+      color: #42b983
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
-  .home
+.home
+  .wrapper
+    .logo
+      img(src="https://upload.wikimedia.org/wikipedia/commons/9/95/600-cell_orthographic-vertex-first-square-sym-2D.svg")
     .brand-name Polytope Solutions
+    .slogan Gain Higher Perspectives
+  .footer Copyright Skyler Fly-Wilson 2018
 </template>
 
 <script lang="ts">
@@ -16,7 +21,26 @@ export default class Home extends Vue {}
 </script>
 
 <style lang="sass">
-.brand-name
-  font-family: "Sirin Stencil", cursive
-  font-size: 4em
+.home
+  .wrapper
+    height: 100vh
+    display: flex
+    flex-direction: column
+    align-items: center
+    justify-content: center
+    .icon img
+      filter: invert(100%)
+      width: 30%
+    .brand-name
+      font-family: "Sirin Stencil", cursive
+      font-size: 7rem
+    .slogan
+      font-size: 2.5rem
+      margin: 2rem
+  .footer
+    width: 100%
+    position: absolute
+    bottom: 0px
+    text-align: center
+    font-size: 1rem
 </style>
