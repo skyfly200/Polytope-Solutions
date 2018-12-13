3527cdcc1f2c87860e16fe3f1961e7dafd1c3223
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-12 21:50:16 -0700

    only print request body

--- src/lambda/contact.js
+++ src/lambda/contact.js
@@ -1,6 +1,5 @@
 export function handler(event, context, callback) {
-  console.log(event);
-  console.log(context);
+  console.log(event.body);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: "Contact Form!" })
