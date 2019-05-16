369fc776576e8431190bf8a031fb6b8c431ae0e3
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-16 10:55:07 -0600

    add loggin lambda

--- src/lambda/contact.js
+++ src/lambda/contact.js
@@ -15,14 +15,17 @@ const generateResponse = (body, statusCode) => {
 exports.handler = function(event, context, callback) {
   if (event.httpMethod !== "POST") {
     // Only run on POST requests
+    console.log("Bad method:", event.httpMethod);
     return callback(null, generateResponse("Method Not Allowed", 405));
   } else if (!event.body) {
     // complain if event body is empty
+    console.log("Empty request body");
     return callback(null, generateResponse("Invalid Request", 204));
   }
   //-- Make sure we have all required data. Otherwise, complain.
   const data = JSON.parse(event.body);
   if (!data.name || !data.email || !data.needs) {
+    console.log("Missing info");
     return callback(null, generateResponse("Missing Information", 204));
   }
   // build the email object from the request body
@@ -42,10 +45,12 @@ exports.handler = function(event, context, callback) {
   try {
     mailgun.messages().send(email, (error, body) => {
       let resp = generateResponse({ body }, 200);
+      console.log("Email sent", email);
       return callback(null, resp);
     });
   } catch (error) {
     let resp = generateResponse("Server Error", 500);
+    console.error(error);
     return callback(error, resp);
   }
 };
