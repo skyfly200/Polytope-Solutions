5f8515a73b7c0fb24375bdb7322293764fc290af
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-17 00:52:42 -0600

    fix message sent view

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,21 +1,25 @@
 <template lang="pug">
 v-flex#contact-form(sm10 offset-sm1)
-  v-card.pa-3.call-to-action(v-if="!sent" data-aos="zoom-in-up" data-aos-anchor-placement="center-center" data-aos-offset="-200")
-    i.fas.fa-rocket.fa-5x
-    .action-header Let us help you find solutions today!
-    v-form(ref="form" v-model="valid" @submit.prevent="sendContact")
-      v-text-field(name="name" label="Name" v-model="name" :rules="rules.name" placeholder="John Smith" autocomplete="name" required)
-      v-text-field(name="company" label="Company" v-model="company" placeholder="Regolith Redistributors")
-      v-text-field(name="industry" label="Industry" v-model="industry" placeholder="Aerospace")
-      v-text-field(name="email" type="email" label="Email" v-model="email" :rules="rules.email" placeholder="name@example.com" autocomplete="email" required)
-      v-textarea(name="needs" label="Tell us about your needs" v-model="needs" :rules="rules.needs" placeholder="Our company needs help with..." auto-grow required)
-      v-btn(type="submit" :disabled="!valid" @click.prevent="sendContact") Send
-      v-btn(@click="clearForm"  :disabled="!name && !company && !industry && !email && !needs") Reset
-      v-alert(:value="alert" :type="alertType" dismissible) {{ alert }}
-  v-card#contact-form.pa-3.form-confirmation(v-else)
-    .action-header Thanks for reaching out! We will be in touch very soon.
-    .response {{ response }}
-    v-btn(@click="sent = false") Back
+  v-card.pa-3(v-if="!sent" data-aos="zoom-in-up" data-aos-anchor-placement="center-center")
+    v-card-text
+      .call-to-action
+        i.fas.fa-rocket.fa-5x
+        .header Let us help you find solutions today!
+      v-form(ref="form" v-model="valid" @submit.prevent="sendContact")
+        v-text-field(name="name" label="Name" v-model="name" :rules="rules.name" placeholder="John Smith" autocomplete="name" required)
+        v-text-field(name="company" label="Company" v-model="company" placeholder="Regolith Redistributors")
+        v-text-field(name="industry" label="Industry" v-model="industry" placeholder="Aerospace")
+        v-text-field(name="email" type="email" label="Email" v-model="email" :rules="rules.email" placeholder="name@example.com" autocomplete="email" required)
+        v-textarea(name="needs" label="Tell us about your needs" v-model="needs" :rules="rules.needs" placeholder="Our company needs help with..." auto-grow required)
+        v-btn(type="submit" :disabled="!valid" @click.prevent="sendContact") Send
+        v-btn(@click="clearForm"  :disabled="!name && !company && !industry && !email && !needs") Reset
+        v-alert(:value="alert" :type="alertType" dismissible) {{ alert }}
+  v-card.pa-3.form-confirmation(v-else data-aos="zoom-in-up" data-aos-anchor-placement="top-center")
+    v-card-title
+      h1 Thanks for reaching out! We will be in touch very soon.
+    v-card-actions
+      v-spacer
+      v-btn(@click="sent = false") Back
 </template>
 
 <script lang="ts">
@@ -41,7 +45,6 @@ export default class ContactForm extends Vue {
   alert: string = "";
   alertType: string = "info";
   sent: boolean = false;
-  response: (AxiosResponse | null) = null;
 
   sendContact(): void {
     if (this.valid) {
@@ -57,7 +60,8 @@ export default class ContactForm extends Vue {
         .then(function(response) {
           console.log(response);
           t.sent = true;
-          t.response = response;
+          let form = t.$refs.form as HTMLFormElement;
+          form.reset();
           t.alert = "Message Sent";
           t.alertType = "success";
         })
@@ -84,7 +88,18 @@ export default class ContactForm extends Vue {
 
 <style lang="sass" scoped>
 #contact-form
+  display: flex
+  align-items: center
+  justify-content: center
   padding: 10vh 0
-.action-header
-  font-size: 2.5rem
+  min-height: 100vh
+  .v-card
+    width: 100%
+    min-height: 600px
+  .header
+    font-size: 2.5rem
+  .form-confirmation
+    display: flex
+    flex-direction: column
+    justify-content: space-between
 </style>
