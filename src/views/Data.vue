f892dba052a1b768e079618904e2e7bee566b42d
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-17 02:49:17 -0600

    add data from home cards to sulutions pages

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -4,8 +4,18 @@
     v-flex(sm10 offset-sm1)
       v-card.pa3.data-card
         v-card-title
-          h1 Data Solutions
-        h3 More info soon to come
+          h1 {{ title }}
+        v-divider
+        v-card-text
+          v-layout.pa3(wrap)
+            v-flex.description(xl6 lg12)
+              h2 Description
+              p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              v-img(:src="img")
+            v-flex.features(xl6 lg12)
+              h2 List
+              ul.list
+                li(v-for="i in list") {{ i }}
   Footer
 </template>
 
@@ -16,7 +26,11 @@ import Footer from "@/components/Footer.vue";
 @Component({
   components: { Footer }
 })
-export default class Data extends Vue {}
+export default class Data extends Vue {
+  img:string = "http://lorempixel.com/640/480/abstract/3";
+  title:string = "Brand";
+  list:Array<string> = ["Item 1", "Item 2", "Item 3", "Item 4"];
+}
 </script>
 
 <style lang="sass" scoped>
